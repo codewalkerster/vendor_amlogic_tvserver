94a5ad005544677c4e812e1f977b02f35e57b840
fei.zhang1 <fei.zhang1@amlogic.com>
2024-04-10 09:57:07 +0800

    tvserver: Modify resource conflicts caused by slow close frontend [1/1]
    PD#SWPL-161958
    
    Problem:
    DTV and ATV sources are switched to each other,
    and the channel cannot be played
    
    Solution:
    Modify resource conflicts caused by slow close frontend
    
    Verify:
    T5M
    
    Change-Id: Id25922cbd3d07f05b287c2539af0bd384f0a4c11
    Signed-off-by: fei.zhang1 <fei.zhang1@amlogic.com>

--- libtv/tv/CTv.cpp
+++ libtv/tv/CTv.cpp
@@ -1244,7 +1244,6 @@ int CTv::playDtvProgramUnlocked (const char *feparas, int mode, int freq, int pa
     }
 
     CFrontEnd::FEParas fp(feparas);
-
     SetSourceSwitchInputLocked(m_source_input_virtual, SOURCE_DTV);
 
     FeMode = fp.getFEMode().getBase();
@@ -2116,6 +2115,7 @@ int CTv::StopTvLock ( void )
 
         /* release ATV/DTV early */
         mFrontDev->setMode(TV_FE_AUTO);
+        mFrontDev->Close();
         tryReleasePlayer(false, m_source_input);
         stopPlaying(false);
 
@@ -2149,7 +2149,6 @@ int CTv::StopTvLock ( void )
             LOGE("%s Set CurrentSourceInfo error!\n", __FUNCTION__);
         }
 
-        mFrontDev->Close();
         mTvAction &= ~TV_ACTION_STOPING;
         mTvStatus = TV_STOP_ED;
         MnoNeedAutoSwitchToMonitorMode = false;
